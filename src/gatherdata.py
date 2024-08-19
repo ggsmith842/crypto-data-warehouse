d0ac114fbcf04040f4f05d570cb74f969e0c9a4c
ggsmith842 <grant.oferrell.smith@gmail.com>
2024-08-18 20:57:04 -0600

    add updates

--- src/gatherdata.py
+++ src/gatherdata.py
@@ -2,27 +2,40 @@ import duckdb
 import asyncio
 from sources.coinbase_api import get_candle_period
 
-async def get_data(n:int):
+
+async def get_data(n: int):
     data = await get_candle_period(n)
     return data
 
 
 if __name__ == "__main__":
 
-    #seems to be an issue trying to execute more than 16 at once
-    data = asyncio.run(get_data(n=16))
+    # seems to be an issue trying to execute more than 16 at once
+    data = asyncio.run(get_data(n=15))
     print(data.shape)
 
     try:
-        data.write_parquet("data\\candle_data.parquet") 
+        data.write_parquet("data\\candle_data.parquet")
 
         with duckdb.connect("data\\cdw.db") as conn:
-            query = """CREATE OR REPLACE TABLE candles AS 
-                            SELECT
-                                * 
-                            FROM 
-                                parquet_scan('data\\candle_data.parquet')
+            table_exists = conn.execute(
+                f"SELECT * FROM information_schema.tables WHERE table_name='CANDLES'"
+            ).fetchone()
+
+            if table_exists:
+                query = """INSERT INTO 
+                            CANDLES 
+                                SELECT 
+                                * FROM parquet_scan('data\\candle_data.parquet')
                     """
+
+            else:
+                query = """CREATE TABLE IF NOT EXISTS CANDLES AS 
+                                SELECT
+                                    * 
+                                FROM 
+                                    parquet_scan('data\\candle_data.parquet')
+                        """
             duckdb.sql(query, connection=conn)
     except Exception as e:
-        print('Error writing to target: ', e)
+        print("Error writing to target: ", e)
