edfc1c3bcdbbb0afb76d2c9623c4ad7905c638ff
ggsmith842 <grant.oferrell.smith@gmail.com>
2024-08-19 21:04:44 -0600

    add emmissions function

--- src/sources/carbonindex.py
+++ src/sources/carbonindex.py
@@ -1,6 +1,17 @@
 import requests
-import re
-from bs4 import BeautifulSoup
 
-url = "https://indices.carbon-ratings.com/"
 
+def get_emissions_data(ticker: str, key: str):
+    
+    url = "https://v2.api.carbon-ratings.com/currencies/{ticker}/emissions/network?key={key}"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+
+      
+
+    
